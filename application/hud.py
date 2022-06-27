fb54ffce7e8efb72ae3ced32693dc0176a4a696e
Patra, Raj <rajpatra.kishore@gmail.com>
2022-06-27 18:08:51 +0530

    [chore]: Re-arranged iexe widgets

--- application/hud.py
+++ application/hud.py
@@ -241,10 +241,10 @@ class HUD:
         for action in self.side_bar["actions"]:
             action.pack(side=TOP, fill=BOTH, expand=0)
 
-        self.iexe_widgets["query_entry"].pack(side=LEFT, fill=BOTH, expand=1)
-        self.iexe_widgets["execute_button"].pack(side=TOP, fill=BOTH, expand=1)
-        self.iexe_widgets["search_button"].pack(side=TOP, fill=BOTH, expand=1)
-        self.iexe_widgets["wiki_button"].pack(side=TOP, fill=BOTH, expand=1)
+        self.iexe_widgets["query_entry"].pack(side=TOP, fill=BOTH, expand=1)
+        self.iexe_widgets["search_button"].pack(side=LEFT, fill=BOTH, expand=1)
+        self.iexe_widgets["execute_button"].pack(side=LEFT, fill=BOTH, expand=1)
+        self.iexe_widgets["wiki_button"].pack(side=LEFT, fill=BOTH, expand=1)
 
         self.canvas_widgets["frame"].pack(side=TOP, fill=BOTH, expand=1)
         self.canvas_widgets["canvas"].pack(side=TOP, fill=BOTH, expand=1)
