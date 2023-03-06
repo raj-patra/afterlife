b09b59bd784ed02f48245e8d8a1b8cef68336491
Patra, Raj <rajpatra.kishore@gmail.com>
2023-03-06 20:17:48 +0530

    [feature::removal]: Updated actions

--- application/helpers/commands.py
+++ application/helpers/commands.py
@@ -34,32 +34,32 @@ ACTIONS = {
         dict(event="start_app", label="Command\nPrompt", query="start cmd /k cd /d %USERPROFILE%\Desktop"),
     ],
     1: [
-        dict(event="open_url", label="Google", query="www.google.com"),
-        dict(event="open_url", label="Gmail", query="mail.google.com"),
-        dict(event="open_url", label="Youtube", query="www.youtube.com"),
-        dict(event="open_url", label="Maps", query="maps.google.com"),
-        dict(event="open_url", label="Keep", query="keep.google.com"),
-    ],
-    2: [
-        dict(event="open_url", label="Docs", query="docs.new"),
-        dict(event="open_url", label="Sheets", query="sheets.new"),
-        dict(event="open_url", label="Slides", query="slides.new"),
-        dict(event="start_app", label="Notepad", query="start notepad"),
-        dict(event="start_app", label="Sticky Notes", query="start explorer.exe shell:appsFolder\Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe!App"),
-    ],
-    3: [
         dict(event="open_url", label="Web\nUtilities", query="www.123apps.com"),
         dict(event="open_url", label="Cloud File\nConverter", query="www.cloudconvert.com"),
         dict(event="open_url", label="Digital PDF\nTools", query="www.smallpdf.com/pdf-tools"),
         dict(event="open_url", label="Online Photo\nEditor", query="www.photopea.com"),
         dict(event="open_url", label="Net\nSpeed", query="www.speedtest.net"),
     ],
-    4: [
+    2: [
         dict(event="open_url", label="Spotify", query="open.spotify.com"),
         dict(event="open_url", label="Bored\nButton", query="www.boredbutton.com/random"),
         dict(event="open_url", label="Wikipedia", query="www.wikipedia.org"),
         dict(event="open_url", label="Library\nGenesis", query="libgen.rs/index.php"),
         dict(event="open_url", label="Good Reads", query="www.readsomethinggreat.com"),
+    ],
+    3: [
+        dict(event="execute_subprocess", label="System\nInfo", query="systeminfo"),
+        dict(event="execute_subprocess", label="Running\nProcesses", query="tasklist"),
+        dict(event="execute_subprocess", label="Environment\nVariables", query="set"),
+        dict(event="execute_subprocess", label="Available\nDrivers", query="driverquery"),
+        dict(event="start_app", label="Notepad", query="start notepad"),
+    ],
+    4: [
+        dict(event="start_app", label="Run", query="start explorer.exe Shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}"),
+        dict(event="start_app", label="God\nMode", query="start explorer.exe Shell:::{ED7BA470-8E54-465E-825C-99712043E01C}"),
+        dict(event="start_app", label="Device\nManagement", query="start devmgmt"),
+        dict(event="start_app", label="Disk\nManagement", query="start diskmgmt"),
+        dict(event="start_app", label="Registry\nEditor", query="start regedit"),
     ]
 }
 
@@ -73,20 +73,6 @@ MENUS = {
         dict(event="execute_subprocess", label="Network Connections", query="netstat -an"),
         dict(event="execute_subprocess", label="IP Configurations", query="ipconfig /allcompartments /all"),
     ],
-    "Advanced": [
-        dict(event="execute_subprocess", label="System Info", query="systeminfo"),
-        dict(event="execute_subprocess", label="Running Processes", query="tasklist"),
-        dict(event="execute_subprocess", label="Environment Variables", query="set"),
-        dict(event="execute_subprocess", label="Available Drivers", query="driverquery"),
-        "---",
-        dict(event="start_app", label="Installed Apps", query="start cmd /k wmic product get name,version"),
-        "---",
-        dict(event="start_app", label="Run", query="start explorer.exe Shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}"),
-        dict(event="start_app", label="God Mode", query="start explorer.exe Shell:::{ED7BA470-8E54-465E-825C-99712043E01C}"),
-        dict(event="start_app", label="Device Management", query="start devmgmt"),
-        dict(event="start_app", label="Disk Management", query="start diskmgmt"),
-        dict(event="start_app", label="Registry Editor", query="start regedit"),
-    ],
     "Socials": [
         dict(event="open_url", label="Facebook", query="www.facebook.com"),
         dict(event="open_url", label="Instagram", query="www.instagram.com"),
@@ -95,7 +81,7 @@ MENUS = {
         "---",
         dict(event="open_url", label="Telegram", query="web.telegram.org"),
         dict(event="open_url", label="Whatsapp", query="web.whatsapp.com"),
-        dict(event="open_url", label="Discord", query="wwwdiscord.com/app"),
+        dict(event="open_url", label="Discord", query="www.discord.com/app"),
         dict(event="open_url", label="Slack", query="www.slack.com"),
         "---",
         dict(event="open_url", label="Tumblr", query="www.tumblr.com"),
